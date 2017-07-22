a0b520444f12ef0a9419cf759b977052b7601fd1
Peter Lesslie <pclesslie@gmail.com>
2017-07-22 16:16:01 -0500

    Add timing to perft

--- main.c
+++ main.c
@@ -3,14 +3,30 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <time.h>
 #include "magic_tables.h"
 #include "move.h"
 #include "position.h"
 #include "movegen.h"
 #include "perft.h"
 
+static struct timespec timediff(struct timespec start, struct timespec end) {
+    struct timespec result;
+    if ((end.tv_nsec - start.tv_nsec) < 0) {
+        result.tv_sec = end.tv_sec - start.tv_sec - 1;
+        result.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    } else {
+        result.tv_sec = end.tv_sec - start.tv_sec;
+        result.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return result;
+}
+
 static void time_test(int depth) {
     uint64_t nodes = 0;
+    struct timespec begin;
+    struct timespec end;
+    struct timespec dur;
     struct position pos;
     const char *fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     if (position_from_fen(&pos, fen) != 0) {
@@ -21,8 +37,12 @@ static void time_test(int depth) {
         fprintf(stderr, "validate_position failed\n");
         exit(EXIT_FAILURE);
     }
+    clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
     nodes = perft_speed(&pos, depth);
-    printf("Depth %d, nodes = %" PRIu64 "\n", depth, nodes);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    dur = timediff(begin, end);
+    printf("Depth %d, nodes = %" PRIu64 " took %ld seconds %ld millis\n",
+            depth, nodes, dur.tv_sec, dur.tv_nsec / 1000000);
 }
 
 int main(int argc, char **argv) {
