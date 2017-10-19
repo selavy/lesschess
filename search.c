300bcdb0dc3a149ac6856f6c708e8f95261e059b
Peter Lesslie <pclesslie@gmail.com>
2017-10-18 21:00:00 -0500

    Rename alpha-beta search functions

--- search.c
+++ search.c
@@ -13,8 +13,9 @@
 struct tt_entry {
     // TODO(plesslie): don't need to save entire hash, just
     // save bits not in the index
-    uint64_t h;
-    int v;
+    uint64_t hash;
+    int value;
+    int depth;
     // TODO(plesslie): probably need to save depth to?  score
     // from a deeper position should overwrite shallower values?
 };
@@ -27,28 +28,37 @@ struct tt_entry tt_tbl[TTSZ];
 void transposition_table_init() {
     assert(__builtin_popcount(TTSZ) == 0);
     for (int i = 0; i < TTSZ; ++i) {
-        tt_tbl[i].h = 0;
-        tt_tbl[i].v = 0;
+        tt_tbl[i].hash = 0;
+        tt_tbl[i].value = 0;
+        tt_tbl[i].depth = 0;
     }
 }
 
-size_t tt_index(uint64_t h) { return h & (TTSZ - 1); }
+// size_t tt_index(uint64_t h) { return h & (TTSZ - 1); }
+size_t tt_index(uint64_t h) { return h % TTSZ; }
 
-int tt_occupied(size_t idx) { return tt_tbl[idx].h != 0 && tt_tbl[idx].v != 0; }
+// 01 function alphabeta(node, depth, α, β, maximizingPlayer)
+// 02      if depth = 0 or node is a terminal node
+// 03          return the heuristic value of node
+// 04      if maximizingPlayer
+// 05          v := -∞
+// 06          for each child of node
+// 07              v := max(v, alphabeta(child, depth – 1, α, β, FALSE))
+// 08              α := max(α, v)
+// 09              if β ≤ α
+// 10                  break (* β cut-off *)
+// 11          return v
+// 12      else
+// 13          v := +∞
+// 14          for each child of node
+// 15              v := min(v, alphabeta(child, depth – 1, α, β, TRUE))
+// 16              β := min(β, v)
+// 17              if β ≤ α
+// 18                  break (* α cut-off *)
+// 19          return v
 
-int tt_value(size_t idx) {
-    assert(tt_tbl[idx].h != 0 && tt_tbl[idx].v != 0);
-    return tt_tbl[idx].v;
-}
-
-void tt_set_value(size_t idx, uint64_t h, int v) {
-    // const size_t idx = tt_index(h);
-    tt_tbl[idx].h = h;
-    tt_tbl[idx].v = v;
-}
-
-int alphabeta_helper(struct position *restrict pos, uint64_t zhash, int depth,
-                     int alpha, int beta, int maximizing) {
+int alphabeta(struct position *restrict pos, uint64_t zhash, int depth,
+              int alpha, int beta, int maximizing) {
     int best;
     int nmoves;
     int i;
@@ -73,7 +83,7 @@ int alphabeta_helper(struct position *restrict pos, uint64_t zhash, int depth,
         best = NEG_INFINITI;
         for (i = 0; i < nmoves; ++i) {
             zhash = make_move(pos, &sp, moves[i], zhash);
-            value = alphabeta_helper(pos, zhash, depth - 1, alpha, beta, 0);
+            value = alphabeta(pos, zhash, depth - 1, alpha, beta, 0);
             zhash = undo_move(pos, &sp, moves[i], zhash);
             best = MAX(best, value);
             alpha = MAX(alpha, best);
@@ -85,7 +95,7 @@ int alphabeta_helper(struct position *restrict pos, uint64_t zhash, int depth,
         best = INFINITI;
         for (i = 0; i < nmoves; ++i) {
             zhash = make_move(pos, &sp, moves[i], zhash);
-            value = alphabeta_helper(pos, zhash, depth - 1, alpha, beta, 1);
+            value = alphabeta(pos, zhash, depth - 1, alpha, beta, 1);
             zhash = undo_move(pos, &sp, moves[i], zhash);
             best = MIN(best, value);
             beta = MIN(beta, best);
@@ -98,7 +108,7 @@ int alphabeta_helper(struct position *restrict pos, uint64_t zhash, int depth,
     return best;
 }
 
-int alphabeta(struct position *restrict pos, const move *restrict moves,
+int alphabeta_search(struct position *restrict pos, const move *restrict moves,
               int nmoves, uint64_t zhash, int depth, int *score) {
     struct savepos sp;
     int best = pos->wtm == WHITE ? NEG_INFINITI - 1 : INFINITI + 1;
@@ -109,8 +119,7 @@ int alphabeta(struct position *restrict pos, const move *restrict moves,
     if (pos->wtm == WHITE) {
         for (i = 0; i < nmoves; ++i) {
             make_move(pos, &sp, moves[i], 0);
-            value =
-                alphabeta_helper(pos, zhash, depth, NEG_INFINITI, INFINITI, 0);
+            value = alphabeta(pos, zhash, depth, NEG_INFINITI, INFINITI, 0);
             if (value > best) {
                 bestmoveno = i;
                 best = value;
@@ -120,8 +129,7 @@ int alphabeta(struct position *restrict pos, const move *restrict moves,
     } else {
         for (i = 0; i < nmoves; ++i) {
             make_move(pos, &sp, moves[i], 0);
-            value =
-                alphabeta_helper(pos, zhash, depth, NEG_INFINITI, INFINITI, 1);
+            value = alphabeta(pos, zhash, depth, NEG_INFINITI, INFINITI, 1);
             if (value < best) {
                 bestmoveno = i;
                 best = value;
@@ -136,10 +144,10 @@ int alphabeta(struct position *restrict pos, const move *restrict moves,
 }
 
 move search(const struct position *restrict const p, int *score, int *searched_depth) {
+    const int max_depth = 5;
     struct position pos;
     move moves[MAX_MOVES];
     int nmoves;
-    const int max_depth = 5;
     uint64_t zhash;
     int moveno;
     int depth;
@@ -153,7 +161,7 @@ move search(const struct position *restrict const p, int *score, int *searched_d
 
     for (depth = 2; depth <= max_depth; ++depth) {
         *searched_depth = depth;
-        moveno = alphabeta(&pos, &moves[0], nmoves, zhash, depth, score);
+        moveno = alphabeta_search(&pos, &moves[0], nmoves, zhash, depth, score);
         if (*score == INFINITI || *score == NEG_INFINITI) {
             break;
         }
