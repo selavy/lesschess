ade8ab8312597454e07055e1fc5ab1f3b3d351fd
Peter Lesslie <pclesslie@gmail.com>
2017-10-21 19:14:36 -0500

    Fix not passing zobrist hash into alphabeta()

--- search.c
+++ search.c
@@ -111,25 +111,26 @@ int alphabeta(struct position *pos, uint64_t zhash, int depth, int alpha, int be
     return best;
 }
 
-
 move alphabeta_search(struct search_node *n, int depth, int *score) {
-    int value;
-    int i;
     struct position *pos = n->pos;
     struct savepos *sp = n->sp;
     const move *moves = n->moves;
-    uint64_t zhash = n->zhash;
+    const uint64_t orig_zhash = n->zhash;
     const int nmoves = n->nmoves;
     move bestmove = INVALID_MOVE;
     int bestscore = pos->wtm == WHITE ? NEG_INFINITI : INFINITI;
+    int value;
+    int i;
+    int zhash = orig_zhash;
 
     assert(nmoves > 0);
 
     if (pos->wtm == WHITE) {
         for (i = 0; i < nmoves; ++i) {
-            make_move(pos, sp, moves[i], 0);
+            zhash = make_move(pos, sp, moves[i], zhash);
             value = alphabeta(pos, zhash, depth, NEG_INFINITI, INFINITI, 0);
-            undo_move(pos, sp, moves[i], 0);
+            zhash = undo_move(pos, sp, moves[i], zhash);
+            assert(zhash == orig_zhash);
             if (value > bestscore) {
                 bestmove = moves[i];
                 bestscore = value;
@@ -137,9 +138,10 @@ move alphabeta_search(struct search_node *n, int depth, int *score) {
         }
     } else {
         for (i = 0; i < nmoves; ++i) {
-            make_move(pos, sp, moves[i], 0);
+            zhash = make_move(pos, sp, moves[i], zhash);
             value = alphabeta(pos, zhash, depth, NEG_INFINITI, INFINITI, 1);
-            undo_move(pos, sp, moves[i], 0);
+            zhash = undo_move(pos, sp, moves[i], zhash);
+            assert(zhash == orig_zhash);
             if (value < bestscore) {
                 bestmove = moves[i];
                 bestscore = value;
@@ -152,12 +154,76 @@ move alphabeta_search(struct search_node *n, int depth, int *score) {
     return bestmove;
 }
 
+#if 0
 
-// int alphabeta_max(struct search_node *n, int alpha, int beta, int depth_left) {
-//     if (depth_left == 0) {
-//         return eval
-//     }
-// }
+int alphabeta_max(struct position *pos, uint64_t orig_zhash, int depth_left, int alpha, int beta) {
+    if (depth_left == 0) {
+        return evaluate(pos);
+    }
+    uint64_t zhash = orig_zhash;
+    move moves[MAX_MOVES];
+    struct savepos sp;
+    const int nmoves = generate_legal_moves(pos, &moves[0]);
+    for (int i = 0; i < nmoves; ++i) {
+        zhash = make_move(pos, &sp, moves[i], zhash);
+        const int score = alphabeta_min(pos, alpha, beta, depth_left - 1);
+        if (score >= beta) {
+            return beta; // fail hard beta cutoff
+        }
+        if (score > alpha) {
+            alpha = score;
+        }
+        zhash = undo_move(pos, &sp, moves[i], zhash);
+        assert(zhash == orig_zhash);
+    }
+
+    return alpha;
+}
+
+int alphabeta_min(struct position *pos, uint64_t orig_zhash, int depth_left, int alpha, int beta) {
+    if (depth_left == 0) {
+        return -evaluate(pos);
+    }
+    uint64_t zhash = orig_zhash;
+    move moves[MAX_MOVES];
+    struct savepos sp;
+    const int nmoves = generate_legal_moves(pos, &moves[0]);
+    for (int i = 0; i < nmoves; ++i) {
+        zhash = make_move(pos, &sp, moves[i], )
+    }
+
+}
+
+move alphabeta_search(struct search_node *n, int depth, int *score) {
+    int value;
+    int i;
+    struct position *pos = n->pos;
+    struct savepos *sp = n->sp;
+    const move *moves = n->moves;
+    const uint64_t orig_zhash = n->zhash;
+    const int nmoves = n->nmoves;
+    move bestmove = INVALID_MOVE;
+    int bestscore = pos->wtm == WHITE ? NEG_INFINITI : INFINITI;
+    uint64_t zhash = orig_zhash;
+
+    assert(nmoves > 0);
+    for (i = 0; i < nmoves; ++i) {
+        zhash = make_move(pos, sp, moves[i], zhash);
+        value = alphabeta(pos, zhash, depth, NEG_INFINITI, INFINITI);
+        zhash = undo_move(pos, sp, moves[i], zhash);
+        assert(zhash == orig_zhash);
+        if (value > bestscore) {
+            bestmove = moves[i];
+            bestscore = value;
+        }
+    }
+
+    assert(bestmove != INVALID_MOVE);
+    *score = bestscore;
+    return bestmove;
+}
+
+#endif
 
 move search(const struct position *p, int *score, int *searched_depth) {
     const int max_depth = 5;
