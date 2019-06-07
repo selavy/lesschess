80f37adc6a10c6f24dad1584147b2aff46b57112
Peter Lesslie <pclesslie@gmail.com>
2019-06-07 18:50:50 +0200

    Add more checks in perft

--- src/perft.cpp
+++ src/perft.cpp
@@ -5,11 +5,12 @@
 namespace lesschess {
 
 u64 perft_speed(Position& position, int depth) {
-    // Move moves[MAX_MOVES];
+    assert(depth >= 0);
     if (depth == 0) {
         return 1;
     }
 
+    // XXX: consider putting on stack; or try to avoid the memory allocation
     std::vector<Move> moves(256);
     int nmoves = position.generate_legal_moves(&moves[0]);
     if (depth == 1) {
@@ -19,9 +20,15 @@ u64 perft_speed(Position& position, int depth) {
     uint64_t nodes = 0;
     Savepos sp;
     for (int i = 0; i < nmoves; ++i) {
+#ifndef NDEBUG
+        auto orig_position = position;
+#endif
         position.make_move(sp, moves[i]);
         nodes += perft_speed(position, depth - 1);
         position.undo_move(sp, moves[i]);
+#ifndef NDEBUG
+        assert(position == orig_position);
+#endif
     }
     return nodes;
 }
